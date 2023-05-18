d113245b5ae448d8d58a6a9a946fdb9860d872a2
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-05-18 13:53:31 +0200

    Add files via upload

---
+++ Interface/IEmployees.cs
@@ -0,0 +1,13 @@
+﻿using JWTAUTH.API.Models;
+namespace JWTAUTH.API.Interface
+{
+    public interface IEmployees
+    {
+        public List<Employee> GetEmployeeDetails();
+        public Employee GetEmployeeDetails(int id);
+        public void AddEmployee(Employee employee);
+        public void UpdateEmployee(Employee employee);
+        public Employee DeleteEmployee(int id);
+        public bool CheckEmployee(int id);
+    }
+}
