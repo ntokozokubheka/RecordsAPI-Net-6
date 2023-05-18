ecb63c032de9d6cd998f525ea986dde7054c23b3
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-05-18 13:52:00 +0200

    Edits on Repo

---
+++ Controllers/EmployeeControlller.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecordsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecordsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeControlller : ControllerBase
+    {
+        private readonly CompanyContext _dbContext;
+
+        public EmployeeControlller(CompanyContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //Get an Employee Record
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>>GetEmployee()
+        {
+            if(_dbContext == null)
+            {
+                return NotFound();
+            }
+
+            return await _dbContext.Employees.ToListAsync();
+        }
+
+        //Get an Employee Record
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>>GetEmployee(int id)
+        {
+            if (_dbContext == null)
+            {
+                return NotFound();
+            }
+            var employee = await _dbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+
+            _dbContext.Employees.Add(employee);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(Employee),new { id = employee.EmployeeId},employee);
+        }
+
+    }
+}
---
+++ Models/CompanyContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordsAPI.Models;
+
+namespace RecordsAPI.Models
+{
+    public class CompanyContext :DbContext
+    {
+
+        public CompanyContext(DbContextOptions options)
+            : base(options)
+        {
+
+
+        }
+
+        public DbSet<Employee>Employees { get; set; } 
+
+    }
+}
